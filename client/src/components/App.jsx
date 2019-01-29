df394c96cdacfedccca893c5b4f2829a3362bae3
coffeegoddd <dbeaubrown@gmail.com>
2019-01-29 13:08:50 -0800

    Testing react render methodmaking sure its rendering

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,5 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currentClientSocketId: '',
+      isPlayerOne: '',
+      isPlayerTwo: '',
+      playerOneSocketId: '',
+      playerTwoSocketId: '',
+    };
+  }
 
+  render() {
+    return (
+      <div>
+        This is React!
+      </div>
+    );
+  }
+}
 export default App;
